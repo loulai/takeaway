ea73f7c6c97a3de6759d8dd593385c98b0801b9d
Louise <louiselai88@gmail.com>
2014-04-26 14:45:53 +0100

    dish can be expected to have names

--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -2,12 +2,15 @@ require "dish"
 
 describe "dish" do
 	
-	it "have a names" do
+	it "has a names" do
 		dish = Dish.new
-		expect(dish.dish_names).to_not be_empty
+		expect(dish.dish_names).not_to be_empty
 	end
 
-	
+	it "has prices"
+		dish = Dish.new
+		expect(dish.dish_prices).not_to be empty
+	end
 
 
 
